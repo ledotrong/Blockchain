322ec1d1c0be0d76debd8f8db83294b483d712f3
Trong Ledo <trong.ledo136@gmail.com>
2020-05-27 11:37:05 +0700

    Update Genesis block previous hash

--- src/blockchain/BlockChain.js
+++ src/blockchain/BlockChain.js
@@ -85,7 +85,7 @@ class BlockChain {
   }
 
   createGenesisBlock() {
-    return new Block(Date.now(), 'Initial Block', '0');
+    return new Block(Date.now(), 'Initial Block', '');
   }
 
   getLatestBlock() {
